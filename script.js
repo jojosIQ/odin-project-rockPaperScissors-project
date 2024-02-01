ecc5ac1e4773ade2304fd941fbcefbf2c5cf265a
jojosIQ <josephputohe@gmail.com>
2024-02-01 20:57:15 +1300

    Get computer function added
    created the function that randomly selects an outcome from an array. Uses math.random*3 to generate a random number between 0 and 3 and math.floor to round it to an int

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function getComputerChoice (choice){
-    
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomNumber = Math.floor(Math.random() * 3); // Generates a random integer between 0 and 2
+    return choices[randomNumber];
 }
+
